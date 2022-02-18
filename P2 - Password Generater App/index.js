aa451795ae75756801931180ff4f62cef1c5ae5b
Akilandeshwari-Ravishankar <akilandeshwari.ravishankar@gmail.com>
2022-02-18 07:05:42 +0530

    P2 files commit

---
+++ P2 - Password Generater App/index.js
@@ -0,0 +1,26 @@
+let pwd1 = document.getElementById("pwd-1")
+let pwd2 = document.getElementById("pwd-2")
+let pwd3 = document.getElementById("pwd-3")
+let pwd4 = document.getElementById("pwd-4")
+
+function generatePasswordHelper() {
+    let len = 12
+    let pwdChars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let password = ""
+    
+    for(let i=0;i<len;i++){
+        var pwdRandomNumber = Math.floor(Math.random()*pwdChars.length)
+        password += pwdChars.substring(pwdRandomNumber, pwdRandomNumber+1)
+    }
+    
+    return password
+}
+
+function generatePassword() {
+    pwd1.textContent = generatePasswordHelper()
+    pwd2.textContent = generatePasswordHelper()
+    pwd3.textContent = generatePasswordHelper()
+    pwd4.textContent = generatePasswordHelper()
+}
+
+
