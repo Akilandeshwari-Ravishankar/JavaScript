72596b820e61bb39a6effc408edf49da2c7b45bc
Akilandeshwari-Ravishankar <akilandeshwari.ravishankar@gmail.com>
2022-02-21 21:24:38 +0530

    updated copy functionality

--- P2 - Password Generater App/index.js
+++ P2 - Password Generater App/index.js
@@ -3,6 +3,11 @@ let pwd2 = document.getElementById("pwd-2")
 let pwd3 = document.getElementById("pwd-3")
 let pwd4 = document.getElementById("pwd-4")
 
+let pwdEl = document.getElementsByClassName("pwd-copy")
+for(let i=0; i<pwdEl.length; i++) {
+    pwdEl[i].style.display = "none"
+}
+
 function generatePasswordHelper() {
     let len = 12
     let pwdChars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -21,14 +26,22 @@ function generatePassword() {
     pwd2.textContent = generatePasswordHelper()
     pwd3.textContent = generatePasswordHelper()
     pwd4.textContent = generatePasswordHelper()
+    document.getElementsByTagName("hr")[0].removeAttribute("hidden")
+    for(let i=0; i<pwdEl.length; i++) {
+        pwdEl[i].style.display = null
+        console.log(pwdEl[i].style.display)
+    }    
 }
 
-// let copyText = document.querySelector(".copy-text")
-// console.log(copyText)
-
 function copyToClipboard(elId) {
-    let passwordToCopy = document.getElementById(`${elId}`).innerText 
-    navigator.clipboard.writeText(passwordToCopy).then(function(){ 
+    let copyTag = document.getElementById(`${elId}-div`)
+    console.log(copyTag)
+    let passwordToCopy = document.getElementById(`${elId}`).innerText
+    navigator.clipboard.writeText(passwordToCopy).then(function(){
+        copyTag.dataset.tooltip = "Copied!"
+        setTimeout(function(){
+            copyTag.dataset.tooltip = "Copy"
+        }, 2500);
         console.log("copied to clipboard!")
     }, function(err) {
         console.error("copy failed!")
